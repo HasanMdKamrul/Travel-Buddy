27b64750c4c04ead31d368d16c8645c6ee789a49
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-08-20 20:48:15 +0200

    Calculation in modal functionality added

--- js/app.js
+++ js/app.js
@@ -46,6 +46,8 @@ const card = (travelCompanion)=>{
 const bookNowHandler = (travelCompanion)=>{
     const modalBody = document.getElementById('modal-display');
 
+    const travelCompanionStringify = JSON.stringify(travelCompanion);
+
     modalBody.innerHTML = `
     <div class="card bg-dark">
         <img src="${travelCompanion.imageURL}" class="card-img-top" alt="...">
@@ -53,13 +55,46 @@ const bookNowHandler = (travelCompanion)=>{
             <h5 class="card-title text-warning">${travelCompanion.veichle}</h5>
             <p class="card-text text-info">${travelCompanion.description}</p>
             <p><small>Fare Per Kilo: ${travelCompanion.farePerKilo}</small></p></p>
-            <a href="#" class="btn btn-danger text-light">Booking</a>
+            <p><small>Fare: <span id='fare'></span> </small></p></p>
+            <p><small>Tax: <span id='tax'></span> </small></p></p>
+            <p><small>Subtotal: <span id='subtotal'></span> </small></p></p>
+           
+            <div class="input-group flex-nowrap my-5">
+                <input id='number-of-kilo' type="text" class="form-control" placeholder="How-Many-Kilo" aria-label="Username" aria-describedby="addon-wrapping">
+                <input id='number-of-veichle' type="text" class="form-control" placeholder="How-Many-Veichle" aria-label="Username" aria-describedby="addon-wrapping">
+            </div>
+            <a href="#" class="btn btn-danger text-light" onclick='calculateTotalFare(${travelCompanionStringify})'>Booking</a>
         </div>
     </div>
     
     `;
 
-    modalBody.appendChild(div);
+    
+}
+
+
+
+const calculateTotalFare = (travelCompanion) => {
+    const numberOfKilos = parseInt(document.getElementById('number-of-kilo').value);
+    const numberOfVeichles = parseInt(document.getElementById('number-of-veichle').value);
+
+    const totalFare = numberOfKilos * numberOfVeichles * travelCompanion.farePerKilo;
+    const totalTax = totalFare * 10/100;
+    const subtotalValue = totalFare + totalTax;
+
+
+    const fareDisplay = document.getElementById('fare');
+    const tax = document.getElementById('tax');
+    const subtotal = document.getElementById('subtotal');
+
+
+
+
+    fareDisplay.innerText = totalFare;
+    tax.innerText = totalTax;
+    subtotal.innerText = subtotalValue;
+
+
 }
 
 
