811e5c74ae314c05ad502b8c902edcd46bafedd6
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-08-20 19:45:54 +0200

    Initial projects setup with db replica done

---
+++ js/database.js
@@ -0,0 +1,26 @@
+// ** Let's create the data objects here
+// ** Car
+const Car = {
+    veichle: 'Car',
+    imageURL: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1583&q=80",
+    farePerKilo: 4, 
+    capacity: 5,
+    description:"Enojoy the luxarious car ride"
+}
+
+// ** Bus 
+const Bus = {
+    veichle: 'Bus',
+    imageURL: "https://images.unsplash.com/photo-1464219789935-c2d9d9aba644?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YnVzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    farePerKilo: 3, 
+    capacity: 35,
+    description:"Enojoy the luxarious bus ride"
+}
+// ** Bike
+const Bike = {
+    veichle: 'Bike',
+    imageURL: "https://images.unsplash.com/photo-1596440114792-d3e0ae1763f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG1vdG9yY3ljbGV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    farePerKilo: 2, 
+    capacity: 2,
+    description:"Enojoy the luxarious bike ride"
+}
